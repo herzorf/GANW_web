c0000ccbe44fe1bfacd2fe0b7fab186faaf91baf
herzorf <1446450047@qq.com>
2023-04-04 13:08:09 +0800

    重构路由文件

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,16 @@
 import { useState } from 'react'
 import styles from './index.module.css'
 import { Button } from 'antd'
-
-function App() {
+const App: React.FC = () => {
   const [count, setCount] = useState(0)
-
   return (
     <div className={styles.app}>
-      <div className="card">
-        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
-          {count}
-        </Button>
-
-      </div>
-
+      <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+        {count}
+      </Button>
     </div>
   )
+
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
 import './global.css'
 import 'antd/dist/reset.css';
-
+import router from './router';
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
