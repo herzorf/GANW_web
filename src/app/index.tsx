8c0bf90794844b208f43123d523b4013788fa7f1
herzorf <1446450047@qq.com>
2023-04-04 13:36:31 +0800

    路由跳转

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import styles from './index.module.scss'
-import { Button } from 'antd'
 const App: React.FC = () => {
-  const [count, setCount] = useState(0)
   return (
     <div className={styles.app}>
-      <Button type="primary" onClick={() => setCount((count) => count + 1)}>
-        {count}
-      </Button>
+      <Link to={"/home"}>home</Link>
     </div>
   )
 
