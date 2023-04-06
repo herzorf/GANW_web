e20204e4be5ac6461b471d250e528ac48804df79
herzorf <1446450047@qq.com>
2023-04-06 11:16:50 +0800

    app布局

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,37 @@
 import { Link } from 'react-router-dom'
 import styles from './index.module.scss'
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Content, Footer, Header } from 'antd/es/layout/layout';
 const App: React.FC = () => {
-  return (
-    <div className={styles.app}>
-      <Link to={"/home"}>home</Link>
-    </div>
-  )
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
-}
+  return (
+    <Layout className={styles.layout}>
+      <Header>
+        <div className={styles.logo} />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(3).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className={styles.siteLayoutContent} style={{ background: colorBgContainer, height: "100%" }}>
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>©2023 Created by Ant UED</Footer>
+    </Layout>
+  );
+};
 
 export default App
