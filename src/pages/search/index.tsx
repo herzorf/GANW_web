eee812b78f42c12d54107d486f87e4422025a85c
herzorf <1446450047@qq.com>
2023-04-07 13:56:41 +0800

    添加分类目录

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,26 +1,16 @@
 import { Col, Form, Input, Menu, MenuProps, Row, Space } from "antd"
 import styles from "./index.module.scss"
+import classItem from "./classItem";
 const { Search } = Input;
 const SearchPage = () => {
     const [form] = Form.useForm();
+
     const onFinish = () => {
         console.log(form.getFieldsValue());
     };
     const onClick: MenuProps['onClick'] = (e) => {
         console.log('click', e);
     };
-    const classItem = [
-        {
-            label: '警用通信装备',
-            key: '1',
-            children: [
-                {
-                    label: '指挥通信',
-                    key: '1-1',
-                },
-            ],
-        },
-    ];
 
     return (
         <div>
@@ -50,6 +40,11 @@ const SearchPage = () => {
             <Row>
                 <Col span={16} offset={4}>
                     <Menu onClick={onClick} className={styles.menu} style={{ width: 256 }} mode="vertical" items={classItem} />
+                </Col>
+            </Row>
+            <Row>
+                <Col span={16} offset={4}>
+
                 </Col>
             </Row>
 
