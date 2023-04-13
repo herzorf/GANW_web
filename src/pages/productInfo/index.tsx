1206647777de93f8eb227a75338eb74bbcec8c3a
herzorf <1446450047@qq.com>
2023-04-13 09:34:03 +0800

    添加三个点显示

--- src/pages/productInfo/index.tsx
+++ src/pages/productInfo/index.tsx
@@ -29,21 +29,21 @@ const ProductInfo = () => {
         <div className={styles.wrapper}>
             <Space direction="vertical" size="large"  >
                 <Row>
-                    <Col span={6} offset={2}>
+                    <Col span={5} offset={2}>
                         <Image
                             width={300}
                             src={tesla}
                         />
                     </Col>
-                    <Col span={10}>
-                        <Descriptions size="small" column={1} labelStyle={{ fontSize: "18px", width: "100px", textAlign: "right", display: "inline-block" }} contentStyle={{ fontSize: "18px" }} title={<h2>郑州日产汽车 纳瓦拉 四驱 汽油自动 ZN1035UCK6C 尊贵型</h2>} >
+                    <Col flex={0} span={10}>
+                        <Descriptions size="small" column={1} labelStyle={{ fontSize: "18px", width: "100px", textAlign: "right", display: "inline-block" }} contentStyle={{ fontSize: "18px" }} title={<h2 className='overflowTest' style={{ maxWidth: "700px" }}>郑州日产汽车 纳瓦拉 四驱 汽油自动 ZN1035UCK6C 尊贵型</h2>} >
                             <Descriptions.Item label="商品报价">150000</Descriptions.Item>
                             <Descriptions.Item label="市场价格">120000</Descriptions.Item>
                             <Descriptions.Item label="折扣率"> <Red>12%</Red></Descriptions.Item>
                             <Descriptions.Item label="库存"> 50 </Descriptions.Item>
                         </Descriptions>
                     </Col>
-                    <Col span={4}>
+                    <Col span={6}>
                         <Card title="郑州日产汽车销售有限公司" bordered={false} style={{ width: 300 }}>
                             <p> 联系人姓名：<span>景向阳</span> </p>
                             <p> 联系电话：<span>15837116369</span> </p>
