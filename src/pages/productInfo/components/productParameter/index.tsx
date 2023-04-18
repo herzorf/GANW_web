6ce67460447863e85c7218ae5a2cd54870f6308b
herzorf <1446450047@qq.com>
2023-04-18 09:21:19 +0800

    添加商品描述布局

--- src/pages/productInfo/components/productParameter/index.tsx
+++ src/pages/productInfo/components/productParameter/index.tsx
@@ -1,13 +1,16 @@
+import { Descriptions } from "antd"
+import styles from "./index.module.scss"
 interface PropsType {
     content?: string
 }
 
 const ProductParameter = ({ content }: PropsType) => {
     return (
-        <div>
-            {content}
+        <div className={styles.wrapper}>
+            <Descriptions column={1} bordered>
+                <Descriptions.Item label="Product">Cloud Database</Descriptions.Item>
 
-
+            </Descriptions>
         </div>
     )
 }
