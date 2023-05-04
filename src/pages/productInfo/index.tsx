e1bb8e5b61635f8618bce118447b8d2668da2722
herzorf <1446450047@qq.com>
2023-05-04 15:39:40 +0800

    兼容IE11

--- src/pages/productInfo/index.tsx
+++ src/pages/productInfo/index.tsx
@@ -18,11 +18,11 @@ const ProductInfo = () => {
             label: `商品参数`,
             children: <ProductParameter proId={proId} />,
         },
-        {
-            key: '2',
-            label: `商品介绍`,
-            children: <ProductIntroduce functionDesc={productInfo.functionDesc} />,
-        },
+        // {
+        //     key: '2',
+        //     label: `商品介绍`,
+        //     children: <ProductIntroduce functionDesc={productInfo.functionDesc} />,
+        // },
     ];
 
     useLayoutEffect(() => {
@@ -38,39 +38,37 @@ const ProductInfo = () => {
     }, [proId])
     return (
         <div className={styles.wrapper}>
-            <Space direction="vertical" size="large"  >
-                <Row>
-                    <Col span={5} offset={2}>
-                        <Image
-                            width={250}
-                            src={productInfo.logoImg}
-                        />
-                    </Col>
-                    <Col flex={0} span={10}>
-                        <Descriptions size="small" column={1} labelStyle={{ fontSize: "18px", width: "100px", textAlign: "right", display: "inline-block" }}
-                            contentStyle={{ fontSize: "18px" }}
-                            title={<h2 className='overflowTest'
-                                style={{ maxWidth: "700px" }}>
-                                {productInfo.proName}
-                            </h2>} >
-                            <Descriptions.Item label="商品报价">{productInfo.marketPrice}</Descriptions.Item>
-                            <Descriptions.Item label="市场价格">{productInfo.salesPrice}</Descriptions.Item>
-                            <Descriptions.Item label="折扣率"> <Red>{productInfo.discountRate}%</Red></Descriptions.Item>
-                        </Descriptions>
-                    </Col>
-                    <Col span={6}>
-                        <Card title={staffInfo.distributorName} bordered={false} style={{ width: 300 }}>
-                            <p> 联系人姓名：<span>{staffInfo.staffName}</span> </p>
-                            <p> 联系电话：<span>{staffInfo.phone}</span> </p>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={22} offset={2}>
-                        <Tabs defaultActiveKey="1" items={items} />
-                    </Col>
-                </Row>
-            </Space>
+            <Row>
+                <Col span={5} offset={2}>
+                    <Image
+                        width={250}
+                        src={productInfo.logoImg}
+                    />
+                </Col>
+                <Col span={10}>
+                    <Descriptions size="small" column={1} labelStyle={{ fontSize: "18px", width: "100px", textAlign: "right", display: "inline-block" }}
+                        contentStyle={{ fontSize: "18px" }}
+                        title={<h2 className='overflowTest'
+                            style={{ maxWidth: "700px" }}>
+                            {productInfo.proName}
+                        </h2>} >
+                        <Descriptions.Item label="商品报价">{productInfo.marketPrice}</Descriptions.Item>
+                        <Descriptions.Item label="市场价格">{productInfo.salesPrice}</Descriptions.Item>
+                        <Descriptions.Item label="折扣率"> <Red>{productInfo.discountRate}%</Red></Descriptions.Item>
+                    </Descriptions>
+                </Col>
+                <Col span={6}>
+                    <Card title={staffInfo.distributorName} bordered={false} style={{ width: 300 }}>
+                        <p> 联系人姓名：<span>{staffInfo.staffName}</span> </p>
+                        <p> 联系电话：<span>{staffInfo.phone}</span> </p>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={22} offset={2}>
+                    <Tabs defaultActiveKey="1" items={items} />
+                </Col>
+            </Row>
         </div>
     )
 }
